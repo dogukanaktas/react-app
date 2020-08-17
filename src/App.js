27607b6a671767f9fd5ef7696025884c2cb88348
Dogukan Aktas <aktasdogukann@gmail.com>
2020-08-17 17:36:08 +0300

    Make API calls and show data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import LineChart from './LineChart';
+import BarChart from './BarChart';
+import "./index.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [countries, SetCountries] = useState([]);
+  const [cases, SetCases] = useState();
+  const [global, SetGlobal] = useState();
+  const [countryName, SetCountryName] = useState();
+ 
+  const urlGlobal = "https://covid19.mathdro.id/api/";
+  const urlCountries = "https://covid19.mathdro.id/api/countries/";
+
+  useEffect(() => {
+    axios.get(urlGlobal).then((res) => SetGlobal(res.data));
+    axios.get(urlCountries).then((res) => SetCountries(res.data.countries));
+  }, []);
+
+  const showCases = (e) => {
+    const urlCases = `https://covid19.mathdro.id/api/countries/${e.target.value}`;
+    axios.get(urlCases).then((res) => SetCases(res.data));
+    SetCountryName(e.target.value);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {/* {console.log("cases", cases)}
+      {console.log("global", global)} */}
+
+      <div className="caseContainer">
+        <div className="card">
+          <p>Infected</p>
+          <p>
+            {cases ? cases.confirmed.value : global && global.confirmed.value}
+          </p>
+          <p>{global && global.lastUpdate}</p>
+          <p>Number of active cases of COVID-19</p>
+        </div>
+
+        <div className="card">
+          <p>Recovered</p>
+          <p>
+            {cases ? cases.recovered.value : global && global.recovered.value}
+          </p>
+          <p>{global && global.lastUpdate}</p>
+          <p>Number of recoveries from COVID-19</p>
+        </div>
+
+        <div className="card">
+          <p>Deaths</p>
+          <p>{cases ? cases.deaths.value : global && global.deaths.value}</p>
+          <p>{global && global.lastUpdate}</p>
+          <p>Number of deaths caused by COVID-19</p>
+        </div>
+      </div>
+
+      <div className="selectContainer">
+        <select
+          className="dropdown"
+          name="countries"
+          onChange={(e) => showCases(e)}
         >
-          Learn React
-        </a>
-      </header>
+          {countries.map((val, i) => (
+            <option key={i} value={val.name}>
+              {val.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <LineChart />
+      </div>
+      <div>
+      <BarChart countryName={countryName} cases={cases} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
